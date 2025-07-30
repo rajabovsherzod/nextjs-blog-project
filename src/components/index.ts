465ba7105a1d681dfd07f2a8bf32e48344e7069b
radjabov_sherzod <155184029+rajabovsherzod@users.noreply.github.com>
2025-07-30 21:43:56 +0500

    little modify

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,5 @@ export { default as Footer } from './footer/footer';
 export { default as Hero } from './hero/hero';
 export { default as Sidebar} from './sidebar/sidebar'
 export { default as Content } from './content/content'
-export { default as Hydration } from './hydration-pattern/hydration'
 export * from './progress-bar/progress-bar';
 export * from './custom-link/custom-link';
